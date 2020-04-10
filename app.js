2a1e7d6952582c1e178f2422c73734b08462660f
DebanjanDasMhapatra <debanjandm123@gmail.com>
2020-04-10 13:00:14 +0530

    Cors bug

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
